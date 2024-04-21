ae4bb6c05d7917695f3afd6cc312045ab161db69
Daniil <dan1795dan@gmail.com>
2024-04-21 18:16:34 +0300

    add scripts for 'modal'

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -56,6 +56,24 @@ document.querySelector('.next').addEventListener('click', function(){
 
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
+
+      // Modal
+
+      $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+      });
+      // $('.button_mini').on('click', function(){
+      //   $('.overlay, #order').fadeIn('slow');
+      // });
+      $('.button_mini').each(function(i) {
+        $(this).on('click' , function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+        })
+      });
       
     });
     })(jQuery);
